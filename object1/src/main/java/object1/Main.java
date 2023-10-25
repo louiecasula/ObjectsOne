caea7b2a46926382935b5497b91a02c8a0378051
Louie <louie@Zipcoders-MacBook-Pro-3.local>
2023-10-25 15:48:28 -0400

    bababouie

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -5,6 +5,12 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String... args) {
-        Logger logger=Logger.getLogger(Main.class.getName());
-        logger.info("This is a module-using Hello World!");    }
+        Logger logger = Logger.getLogger(Main.class.getName());
+        logger.info("This is a module-using Hello World!");
+
+        Person person1 = new Person("John", 30, 160);
+
+        Car suv = new Car("Chevy", "Equinox", 2005, 200000);
+        System.out.print("This car's year is: " + suv.getYear());
+    }
 }
---
+++ object1/src/main/java/object1/Person.java
@@ -0,0 +1,20 @@
+package object1;
+
+// Define a class
+class Person {
+    String name;
+    int age = 0;
+    int height = 0;
+
+    // Constructor
+    public Person(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height; // in centimeters
+    }
+
+    // Declare and initialize an object
+
+}
+
+
