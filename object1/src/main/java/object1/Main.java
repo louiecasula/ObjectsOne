cf0c37b9c44d91197275dc1ce0610c09f69a307d
Louie <louie@Zipcoders-MBP-3.fios-router.home>
2023-10-25 22:46:44 -0400

    All finished

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -10,7 +10,16 @@ public class Main {
 
         Person person1 = new Person("John", 30, 160);
 
-        Car suv = new Car("Chevy", "Equinox", 2005, 200000);
-        System.out.print("This car's year is: " + suv.getYear());
+        Car SUV = new Car("Chevy", "Equinox", 2005, 200000);
+        String info = String.join(" ", String.valueOf(SUV.getYear()), SUV.getMake(), SUV.getModel());
+        System.out.println("This car is a " + info + " with " + SUV.getMileage() + " miles on it.");
+
+        Chair officeChair = new Chair("satin", "red", 7);
+        System.out.printf("The chair is %s and made of %s with %d legs on it.\n", officeChair.getColor(), officeChair.getMaterial(), officeChair.getLegs());
+
+        Dog dawg = new Dog("Goofy", 25, "cartoon dog.\n");
+        dawg.setAge(47);
+        System.out.printf("My dog's name is %s. He's a %d year old %s", dawg.getName(), dawg.getAge(), dawg.getBreed());
+
     }
 }
